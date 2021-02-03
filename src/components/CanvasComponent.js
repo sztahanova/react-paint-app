743d0785885d88cb13b4947c7e2fac69af53dcfd
sztahanova <sztahanova@github.com>
2021-02-03 19:59:34 +0100

    Canvas refactor

--- src/components/CanvasComponent.js
+++ src/components/CanvasComponent.js
@@ -1,81 +1,74 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import useWindowSize from "./WindowSize";
 
-class Canvas extends React.Component {
-  constructor(props) {
-    super(props);
-    this.canvasRef = React.createRef();
-    this.handleMouseMove = this.handleMouseMove.bind(this);
-    this.handleResize = this.handleResize.bind(this);
-    this.startDrawing = this.startDrawing.bind(this);
-    this.stopDrawing = this.stopDrawing.bind(this);
-    this.state = {
-      drawing: false,
-      width: window.innerWidth,
-    };
-  }
+const Canvas = (props) => {
+  const [drawing, setDrawing] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
 
-  componentDidMount() {
-    this.ctx = this.canvasRef.current.getContext("2d");
-    window.addEventListener("resize", this.handleResize);
-  }
+  const canvasRef = useRef();
+  const ctx = useRef();
 
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.handleResize);
-  }
+  useEffect(() => {
+    ctx.current = canvasRef.current.getContext("2d");
+  }, []);
 
-  handleMouseMove(e) {
+  const [windowWidth, windowHeight] = useWindowSize(() => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  });
+
+  const handleMouseMove = (event) => {
     // actual coordinates
     const coords = [
-      e.clientX - this.canvasRef.current.offsetLeft,
-      e.clientY - this.canvasRef.current.offsetTop,
+      event.clientX - canvasRef.current.offsetLeft,
+      event.clientY - canvasRef.current.offsetTop,
     ];
-    if (this.state.drawing) {
-      this.ctx.lineTo(...coords);
-      this.ctx.stroke();
-    }
-    if (this.props.handleMouseMove) {
-      this.props.handleMouseMove(...coords);
-    }
-  }
 
-  handleResize() {
-    this.setState({ width: window.innerWidth, height: window.innerHeight });
-  }
+    if (drawing) {
+      ctx.current.lineTo(...coords);
+      ctx.current.stroke();
+    }
 
-  startDrawing(e) {
-    this.ctx.lineJoin = "round";
-    this.ctx.lineCap = "round";
-    this.ctx.lineWidth = 10;
-    this.ctx.strokeStyle = this.props.color;
-    this.ctx.beginPath();
+    if (props.handleMouseMove) {
+      props.handleMouseMove(...coords);
+    }
+  };
+
+  const startDrawing = (event) => {
+    ctx.current.lineJoin = "round";
+    ctx.current.lineCap = "round";
+    ctx.current.lineWidth = 10;
+    ctx.current.strokeStyle = props.color;
+    ctx.current.beginPath();
+
     // actual coordinates
-    this.ctx.moveTo(
-      e.clientX - this.canvasRef.current.offsetLeft,
-      e.clientY - this.canvasRef.current.offsetTop
+    ctx.current.moveTo(
+      event.clientX - canvasRef.current.offsetLeft,
+      event.clientY - canvasRef.current.offsetTop
     );
-    this.setState({ drawing: true });
-  }
 
-  stopDrawing() {
-    this.ctx.closePath();
-    this.setState({ drawing: false });
-  }
+    setDrawing(true);
+  };
 
-  render() {
-    return (
-      <React.Fragment>
-        <canvas
-          ref={this.canvasRef}
-          width={this.props.width || this.state.width}
-          height={this.props.height || this.state.height}
-          onMouseDown={this.startDrawing}
-          onMouseUp={this.stopDrawing}
-          onMouseOut={this.stopDrawing}
-          onMouseMove={this.handleMouseMove}
-        />
-      </React.Fragment>
-    );
-  }
+  const stopDrawing = () => {
+    ctx.current.closePath();
+    setDrawing(false);
+  };
+
+  return (
+    <React.Fragment>
+      <canvas
+        ref={canvasRef}
+        width={props.width || width}
+        height={props.height || height}
+        onMouseDown={startDrawing}
+        onMouseUp={stopDrawing}
+        onMouseOut={stopDrawing}
+        onMouseMove={handleMouseMove}
+      />
+    </React.Fragment>
+  );
 };
 
 export default Canvas;
