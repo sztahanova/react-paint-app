45f7e6932399e7e13c197a724ad0f4e90e0ed0f7
sztahanova <sztahanova@github.com>
2021-02-04 20:57:13 +0100

    Fixed for mobile

--- src/components/CanvasComponent.js
+++ src/components/CanvasComponent.js
@@ -10,14 +10,14 @@ const Canvas = (props) => {
   const ctx = useRef();
 
   // needed to persist drawing on resize
-  const inMemCanvasRef = useRef(document.createElement('canvas'));
+  const inMemCanvasRef = useRef(document.createElement("canvas"));
   const inMemCtx = useRef();
 
   useEffect(() => {
     ctx.current = canvasRef.current.getContext("2d");
     inMemCtx.current = inMemCanvasRef.current.getContext("2d");
   }, []);
-  
+
   useEffect(() => {
     ctx.current.fillStyle = "#ffffff";
     ctx.current.fillRect(0, 0, width, height);
@@ -30,10 +30,14 @@ const Canvas = (props) => {
   });
 
   const handleMouseMove = (event) => {
+    drawLine(event.clientX, event.clientY);
+  };
+
+  function drawLine(posX, posY) {
     // actual coordinates
     const coords = [
-      event.clientX - canvasRef.current.offsetLeft,
-      event.clientY - canvasRef.current.offsetTop,
+      posX - canvasRef.current.offsetLeft,
+      posY - canvasRef.current.offsetTop,
     ];
 
     if (drawing) {
@@ -44,22 +48,28 @@ const Canvas = (props) => {
     if (props.handleMouseMove) {
       props.handleMouseMove(...coords);
     }
-  };
+  }
 
-  const startDrawing = (event) => {
+  function startDrawing(posX, posY) {
     ctx.current.lineJoin = "round";
     ctx.current.lineCap = "round";
     ctx.current.lineWidth = props.lineWidth;
     ctx.current.strokeStyle = props.color;
     ctx.current.beginPath();
 
+    console.log(posX, posY);
+
     // actual coordinates
     ctx.current.moveTo(
-      event.clientX - canvasRef.current.offsetLeft,
-      event.clientY - canvasRef.current.offsetTop
+      posX - canvasRef.current.offsetLeft,
+      posY - canvasRef.current.offsetTop
     );
 
     setDrawing(true);
+  }
+
+  const handleMouseDown = (event) => {
+    startDrawing(event.clientX, event.clientY);
   };
 
   const stopDrawing = () => {
@@ -71,16 +81,33 @@ const Canvas = (props) => {
     inMemCtx.current.drawImage(canvasRef.current, 0, 0);
   };
 
+  const handleTouchStart = (e) => {
+    const touch = e.touches[0];
+    startDrawing(touch.clientX, touch.clientY);
+  };
+
+  const handleTouchMove = (e) => {
+    const touch = e.touches[0];
+    drawLine(touch.clientX, touch.clientY);
+  };
+
+  const handleTouchEnd = (e) => {
+    stopDrawing();
+  };
+
   return (
     <React.Fragment>
       <canvas
         ref={canvasRef}
         width={props.width || width}
         height={props.height || height}
-        onMouseDown={startDrawing}
+        onMouseDown={handleMouseDown}
         onMouseUp={stopDrawing}
         onMouseOut={stopDrawing}
         onMouseMove={handleMouseMove}
+        onTouchStart={handleTouchStart}
+        onTouchEnd={handleTouchEnd}
+        onTouchMove={handleTouchMove}
       />
     </React.Fragment>
   );
